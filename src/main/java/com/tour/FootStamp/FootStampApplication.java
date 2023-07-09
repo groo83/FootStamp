863ae7b9ee31a8a11dce167ad8c80248bf666550
pjy <pjy3696@naver.com>
2023-07-09 19:55:17 +0900

    project create

---
+++ src/main/java/com/tour/FootStamp/FootStampApplication.java
@@ -0,0 +1,13 @@
+package com.tour.FootStamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootStampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootStampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tour/FootStamp/FootStampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.FootStamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootStampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
