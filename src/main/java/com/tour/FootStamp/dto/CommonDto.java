7602e935fea0eb0aac3d3555977807c60c68b68f
pjy <pjy3696@naver.com>
2023-07-18 22:23:35 +0900

    공통 dto 생성

---
+++ src/main/java/com/tour/FootStamp/dto/CommonDto.java
@@ -0,0 +1,99 @@
+package com.tour.FootStamp.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CommonDto {
+
+    /*
+     * 지역코드
+     */
+    private String areacode;
+
+    /*
+     * 시군구코드
+     */
+    private String sigungucode;
+
+    /*
+     * 대분류
+     */
+    private String cat1;
+
+    /*
+     * 중분류
+     */
+    private String cat2;
+
+    /*
+     * 소분류
+     */
+    private String cat3;
+
+    /*
+     * 콘텐츠ID
+     */
+    private String contentId;
+
+    /*
+     * 관광타입 ID
+     */
+    private String contentTypeId;
+
+    /*
+     * 주소
+     */
+    private String addr1;
+
+    /*
+     * 상세주소
+     */
+    private String addr2;
+
+    /*
+     * 우편번호
+     */
+    private String zipcode;
+
+    /*
+     * GPS X좌표
+     */
+    private String mapx;
+
+    /*
+     * GPS Y좌표
+     */
+    private String mapy;
+
+    /*
+     * 전화번호
+     */
+    private String tel;
+
+    /*
+     * 콘텐츠명(제목)
+     */
+    private String title;
+
+    /*
+     * 홈페이지주소
+     */
+    private String homepage;
+
+    /*
+     * 대표이미지(원본)
+     */
+    private String firstimage;
+
+    /*
+     * 대표이미지(썸네일)
+     */
+    private String firstimage2;
+
+    /*
+     * 개요
+     */
+    private String overview;
+
+
+}
